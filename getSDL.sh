aaf40e493a8cd11c78662c9830033d34b02ae75b
Algiuxs <146809287+Algiuxs@users.noreply.github.com>
2024-05-16 21:58:42 +0300

    updated getSDL

--- getSDL.sh
+++ getSDL.sh
@@ -7,12 +7,12 @@ pushd app/jni/SDL
 wget https://www.libsdl.org/release/SDL2-${SDL2_VERSION}.zip
 unzip -q SDL2-${SDL2_VERSION}.zip
 mv SDL2-${SDL2_VERSION}/src src
-mv SDL2-${SDL2_VERSION}include include
+mv SDL2-${SDL2_VERSION}/include include
 
 #MSYS2's symlinks seem to just copy the folder which isn't really a good thing recursively. Native symlinks are broken for me so let's just use Windows's implementation
 if [[ -n "$WINDIR" ]]; then
     cmd <<< "mklink /D include\SDL2 ."
-    echo "if mklink failed, you need to excecute the "mklink /D include\SDL2 ." command as administrator in windows command prompt."
+    echo "if mklink failed, you need to excecute the "mklink /D include\SDL2 ." command at the root of the project as administrator in windows command prompt."
 else
     ln -s . include/SDL2
 fi

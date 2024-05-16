ba86e9759c88f5305b74009c8a29b38199df2a70
Algiuxs <146809287+Algiuxs@users.noreply.github.com>
2024-05-16 21:55:38 +0300

    fixed gradle, added higher SDL version.

--- getSDL.sh
+++ getSDL.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+SDL2_VERSION="2.26.5"
+
 pushd app/jni/SDL
 
-wget https://www.libsdl.org/release/SDL2-2.0.12.zip
-unzip -q SDL2-2.0.12.zip
-mv SDL2-2.0.12/src src
-mv SDL2-2.0.12/include include
+wget https://www.libsdl.org/release/SDL2-${SDL2_VERSION}.zip
+unzip -q SDL2-${SDL2_VERSION}.zip
+mv SDL2-${SDL2_VERSION}/src src
+mv SDL2-${SDL2_VERSION}include include
 
 #MSYS2's symlinks seem to just copy the folder which isn't really a good thing recursively. Native symlinks are broken for me so let's just use Windows's implementation
 if [[ -n "$WINDIR" ]]; then
     cmd <<< "mklink /D include\SDL2 ."
+    echo "if mklink failed, you need to excecute the "mklink /D include\SDL2 ." command as administrator in windows command prompt."
 else
     ln -s . include/SDL2
 fi
 
-rm SDL2-2.0.12.zip
-rm -r SDL2-2.0.12
+rm SDL2-${SDL2_VERSION}.zip
+rm -r SDL2-${SDL2_VERSION}
 
 popd
